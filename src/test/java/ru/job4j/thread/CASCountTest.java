fc7626e0be8012cfe279fadadad4de24d83a864a
GrnDev1 <grndev@mail.ru>
2023-08-16 13:55:09 +0300

    0. CAS - операции [#6859]

--- src/test/java/ru/job4j/thread/CASCountTest.java
+++ src/test/java/ru/job4j/thread/CASCountTest.java
@@ -14,4 +14,16 @@ class CASCountTest {
         Thread.sleep(1000);
         assertThat(casCount.get()).isEqualTo(100);
     }
+
+    @Test
+    public void whenIncrementThen2() throws InterruptedException {
+        CASCount casCount = new CASCount();
+        Thread thread1 = new Thread(casCount::increment);
+        Thread thread2 = new Thread(casCount::increment);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        assertThat(casCount.get()).isEqualTo(2);
+    }
 }
